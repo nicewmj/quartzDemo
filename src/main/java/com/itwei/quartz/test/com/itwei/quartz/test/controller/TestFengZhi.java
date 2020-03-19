08184f9d0767a193968d8f15c6c9e1ffb159c97f
v_mjwei <778026675@qq.com>
2020-03-19 16:23:15 +0800

    测试分支

---
+++ src/main/java/com/itwei/quartz/test/com/itwei/quartz/test/controller/TestFengZhi.java
@@ -0,0 +1,11 @@
+package com.itwei.quartz.test.com.itwei.quartz.test.controller;
+
+
+import javax.xml.ws.RequestWrapper;
+
+public class TestFengZhi {
+
+    public String test(){
+        return "测试分支";
+    }
+}
